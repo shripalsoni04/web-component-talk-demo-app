6a850cf4eb4532313896ec7deff0be1b1be8c418
Shripal Soni <shripalsoni04@gmail.com>
2019-05-31 11:05:50 +0530

    step-9: Toggle panel from index page by calling togglePanel method

--- native/index.js
+++ native/index.js
@@ -3,3 +3,7 @@ import './panel.js';
 const panelEle = document.querySelector('sh-panel');
 panelEle.title = 'Custom Panel Title';
 panelEle.addEventListener('toggle', event => console.log(event));
+
+document
+  .querySelector('#toggle-panel-btn')
+  .addEventListener('click', () => panelEle.togglePanel());
